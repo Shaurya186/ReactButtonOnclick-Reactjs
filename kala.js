92d88f0b01157d5be98d4c6c8b3478a8911c9675
SH4URYA.Singh838 <69413964+Shaurya186@users.noreply.github.com>
2020-09-10 23:08:34 +0530

    Add files via upload

---
+++ kala.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Football extends React.Component {
+  shoot() {
+    alert("Great Shot!");
+  }
+  render() {
+    return (
+      <button onClick={this.shoot}>Take the shot!</button>
+    );
+  }
+}
+
+ReactDOM.render(<Football />, document.getElementById('roo
